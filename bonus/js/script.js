12e36a17fa25968b82d7351abc883c6b1b6b7622
MicheleLafronza <michele.lafronza91@gmail.com>
2024-06-25 16:13:30 +0200

    feat: bonus start

---
+++ bonus/js/script.js
@@ -0,0 +1,78 @@
+const slides = [
+{
+        image: 'img/01.webp',
+        title: 'Marvel\'s Spiderman Miles Morale',
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    }, {
+        image: 'img/02.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    }, {
+        image: 'img/03.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    }, {
+        image: 'img/04.webp',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    }, {
+        image: 'img/05.webp',
+        title: "Marvel's Avengers",
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+    }
+];
+
+// console.log(slides);
+
+// creo istanza di app Vue
+const { createApp } = Vue
+
+// definisco tutto quello che comprende l'app
+createApp({
+// inserisco dati
+  data() {
+    return {
+        slides: [
+                    {
+                        image: 'img/01.webp',
+                        title: 'Marvel\'s Spiderman Miles Morale',
+                        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                    }, {
+                        image: 'img/02.webp',
+                        title: 'Ratchet & Clank: Rift Apart',
+                        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                    }, {
+                        image: 'img/03.webp',
+                        title: 'Fortnite',
+                        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                    }, {
+                        image: 'img/04.webp',
+                        title: 'Stray',
+                        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                    }, {
+                        image: 'img/05.webp',
+                        title: "Marvel's Avengers",
+                        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+                    }
+                ],
+        activeItem: 0,
+    }
+  },
+  methods: {
+    // al click su next cambia l'immagine cambia sulla successiva
+    nextImage() {
+        this.activeItem++;
+        if (this.activeItem > this.slides.length - 1) {
+            this.activeItem = 0;
+        }
+    },
+    // al click su prev l'immagine cambia sulla precedente
+    prevImage() {
+        this.activeItem--;
+        if (this.activeItem < 0) {
+            this.activeItem = this.slides.length - 1;
+        }
+    }
+  }
+// monto tutto sull'elemento
+}).mount('#app')
